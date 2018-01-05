811e89ca80f2dacd18dfbd0e71a62e68a917d169
Amrouche Hamza <hamza@les-tilleuls.coop>
2018-01-05 14:50:19 +0100

    reproducer for 25598

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
---
+++ src/Api/AllowEmptyItemIriConverter.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hamza
+ * Date: 05/01/2018
+ * Time: 14:48
+ */
+
+/*
+* This file is part of the ***PROJECT*** project.
+*
+* (c) hamza <hamza>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace App\Api;
+
+
+class AllowEmptyItemIriConverter
+{
+
+}
